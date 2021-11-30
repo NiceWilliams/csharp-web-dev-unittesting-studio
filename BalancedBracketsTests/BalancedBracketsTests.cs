6a929978f9865c5203f22ed07f20060c164d9ca9
AaronW <aaronwhildkhild@yahoo.com>
2021-11-30 16:37:08 -0600

    Completed the tasks

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -13,5 +13,82 @@ namespace BalancedBracketsTests
         {
             Assert.AreEqual(true, true);
         }
+
+        [TestMethod]
+
+        public void TestBrackets()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
+
+        [TestMethod]
+
+        public void TestRightBracket()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]"));
+        }
+
+        [TestMethod]
+
+        public void TestLeftBracket()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+        }
+
+        [TestMethod]
+
+        public void TestTextFull()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[JavaScript]"));
+        }
+
+        [TestMethod]
+
+        public void TestText()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("Java[Script]"));
+        }
+
+        [TestMethod]
+
+        public void TestEmptyLeft()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]JavaScript"));
+        }
+
+        [TestMethod]
+
+        public void TestBackwards()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]["));
+        }
+
+        [TestMethod]
+
+        public void TestDoubleLeft()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[["));
+        }
+
+        [TestMethod]
+
+        public void TestDoubleRight()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]]"));
+        }
+
+        [TestMethod]
+
+        public void TestTripleBack()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]]]Java[[["));
+        }
+
+        [TestMethod]
+
+        public void TestEmptyText()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(""));
+        }
     }
 }
